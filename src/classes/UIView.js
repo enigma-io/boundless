e1d9955ed8c08d982da48956de6f32ccc160d9b2
Evan Jacobs <evan.jacobs@enigma.io>
2015-07-02 14:26:36 -0400

    UIView baseclass

---
+++ src/classes/UIView.js
@@ -0,0 +1,12 @@
+/**
+ * Currently unused, but may add special functionality like instrumentation at a later date.
+ *
+ * @class UIView
+ * @author Evan Jacobs <evan.jacobs@enigma.io>
+ */
+
+import React from 'react';
+
+class UIView extends React.Component {}
+
+export default UIView;
