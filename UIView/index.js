ad9ba11d85bffde467cb1534c74248c738ebdfad
Evan Jacobs <evan.jacobs@enigma.io>
2015-07-27 18:56:14 -0400

    Implement shouldComponentUpdate in UIView
    Approximates PureRenderMixin, as mixins are not available in ES6
    React classes. Long term, this will be replaced with decorators when
    Babel releases stage 2+ support for them.

--- UIView/index.js
+++ UIView/index.js
@@ -7,6 +7,49 @@
 
 import React from 'react';
 
+/*
+ * A simplified version of Alberto Leal's shallowequal module
+ *
+ * https://github.com/Dashed/shallowequal
+ * Author: https://github.com/Dashed
+ * License (MIT): https://github.com/Dashed/shallowequal/blob/master/LICENSE
+ */
+function shallowEqual(objA, objB) {
+    if (objA === objB) {
+        return true;
+    }
+
+    if (typeof objA !== 'object'
+        || objA === null
+        || typeof objB !== 'object'
+        || objB === null) {
+        return false;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+    const len = keysA.length;
+
+    if (len !== keysB.length) {
+        return false;
+    }
+
+    // Test for A's keys different from B.
+    const bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+
+    for (let i = 0; i < len; i++) {
+        const key = keysA[i];
+
+        if (!bHasOwnProperty(key)) {
+            return false;
+        } else if (objA[key] !== objB[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 class UIView extends React.Component {
     constructor(...args) {
         super(...args);
@@ -31,6 +74,18 @@ class UIView extends React.Component {
 
             return c & c;
         }, 0);
+    }
+
+    /**
+     * Approximates the @link{PureRenderMixin https://facebook.github.io/react/docs/pure-render-mixin.html} from ES5 React. Implement shouldComponentUpdate in your subclass to override this functionality.
+     * @public
+     *
+     * @param  {Object} nextProps the incoming props definition, may differ from current props
+     * @param  {Object} nextState the incoming state definition, may differ from current state
+     * @return {Boolean}          Informs React to re-render the component.
+     */
+    shouldComponentUpdate(nextProps, nextState) {
+        return !shallowEqual(nextProps, this.props) || !shallowEqual(nextState, this.state);
     }
 
     /**
