dd440368e3e89ea3258b86d3cc89ea7ace820856
Evan Jacobs <evan.jacobs@enigma.io>
2015-08-14 19:27:56 -0400

    Bump dependencies

--- UITable/unit.jsx
+++ UITable/unit.jsx
@@ -1,15 +1,3 @@
 /* eslint no-unused-expressions:0 */
 
-import UITable from './index.jsx';
-import React from 'react';
-
-function noop() {}
-
-describe('UITable', () => {
-    const sandbox = sinon.sandbox.create();
-
-    afterEach(() => {
-        React.unmountComponentAtNode(document.body);
-        sandbox.restore();
-    });
-});
+describe('UITable', () => {});
