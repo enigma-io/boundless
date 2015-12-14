3fc4aa31802a5e7ebf6a4874d2cd454a859ca106
Evan Jacobs <evan.jacobs@enigma.io>
2015-12-14 18:05:17 -0500

    [Chore][Fix] Don't mangle compressed ES5 component names
    Makes it difficult to debug in React tools.

--- es5/UIUtils/transform/index.js
+++ es5/UIUtils/transform/index.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=function(){for(var r=["transform","WebkitTransform","MozTransform","OTransform","msTransform"],e=0,t=r.length;t>e;e++)if(r[e]in document.documentElement.style)return r[e];return!1}();
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=function(){for(var props=["transform","WebkitTransform","MozTransform","OTransform","msTransform"],i=0,len=props.length;len>i;i++)if(props[i]in document.documentElement.style)return props[i];return!1}();
