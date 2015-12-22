d6f716581a6c727b184895505f9d856409ee50e5
Evan Jacobs <evan.jacobs@enigma.io>
2015-12-22 11:33:07 -0500

    [Chore] Clean up jest config slightly

--- jest.preprocessor.js
+++
@@ -1,9 +0,0 @@
-var babelJest = require('babel-jest');
-
-module.exports = {
-    process: function(src, filename) {
-        return   filename.indexOf('node_modules') === -1
-               ? babelJest.process(src, filename)
-               : src;
-    }
-};
