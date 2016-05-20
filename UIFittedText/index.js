7646210ed2839adc065833c667534fc1b03031b7
Evan Jacobs <evan.jacobs@enigma.io>
2016-05-20 17:38:36 -0400

    UIFittedText: use a shared window resize listener
    Takes the number of window resize listeners from O(n) to O(1) for
    any number of UIFittedText instances on a page.

--- UIFittedText/index.js
+++ UIFittedText/index.js
@@ -12,6 +12,49 @@ function toI(stringNumber) {
     return parseInt(stringNumber, 10);
 }
 
+function rescale(instance) {
+    const node = findDOMNode(instance);
+    const containerBox = window.getComputedStyle(node.parentNode);
+    const fontSize = toI(window.getComputedStyle(node).fontSize);
+
+    let containerHeight = toI(containerBox.height);
+    let containerWidth = toI(containerBox.width);
+
+    if (   containerBox.boxSizing === 'border-box'
+        || containerBox.boxSizing === 'padding-box') { // need to account for padding
+        containerHeight -= toI(containerBox.paddingTop) + toI(containerBox.paddingBottom);
+        containerWidth -= toI(containerBox.paddingLeft) + toI(containerBox.paddingRight);
+    }
+
+    const optimizeForHeight = Math.floor((fontSize / node.offsetHeight) * containerHeight);
+    const optimizeForWidth = Math.floor((fontSize / node.offsetWidth) * containerWidth);
+
+    // the || 1 is a fallback to prevent fontSize from being set to zero, which fubars things
+    node.style.fontSize = (Math.min(instance.props.maxFontSize, optimizeForHeight, optimizeForWidth) || 1) + 'px';
+}
+
+function handleWindowResize() {
+    instances.forEach(instance => rescale(instance));
+}
+
+function registerInstance(instance) {
+    if (instances.length === 0) {
+        window.addEventListener('resize', handleWindowResize, true);
+    }
+
+    instances.push(instance);
+}
+
+function unregisterInstance(instance) {
+    instances.splice(instances.indexOf(instance), 1);
+
+    if (instances.length === 0) {
+        window.removeEventListener('resize', handleWindowResize, true);
+    }
+}
+
+const instances = [];
+
 export default class UIFittedText extends UIView {
     static defaultProps = {
         maxFontSize: Number.MAX_VALUE,
@@ -26,38 +69,19 @@ export default class UIFittedText extends UIView {
     }
 
     componentDidMount() {
-        this.rescale();
+        rescale(this);
 
-        window.addEventListener('resize', this.rescale, true);
+        // there are likely to be multiple instances of this component on a page, so it makes sense to just use
+        // a shared global resize listener instead of each component having its own
+        registerInstance(this);
     }
 
     componentDidUpdate() {
-        this.rescale();
+        rescale(this);
     }
 
     componentWillUnmount() {
-        window.removeEventListener('resize', this.rescale, true);
-    }
-
-    rescale = () => {
-        const node = findDOMNode(this);
-        const containerBox = window.getComputedStyle(node.parentNode);
-        const fontSize = toI(window.getComputedStyle(node).fontSize);
-
-        let containerHeight = toI(containerBox.height);
-        let containerWidth = toI(containerBox.width);
-
-        if (   containerBox.boxSizing === 'border-box'
-            || containerBox.boxSizing === 'padding-box') { // need to account for padding
-            containerHeight -= toI(containerBox.paddingTop) + toI(containerBox.paddingBottom);
-            containerWidth -= toI(containerBox.paddingLeft) + toI(containerBox.paddingRight);
-        }
-
-        const optimizeForHeight = Math.floor((fontSize / node.offsetHeight) * containerHeight);
-        const optimizeForWidth = Math.floor((fontSize / node.offsetWidth) * containerWidth);
-
-        // the || 1 is a fallback to prevent fontSize from being set to zero, which fubars things
-        node.style.fontSize = (Math.min(this.props.maxFontSize, optimizeForHeight, optimizeForWidth) || 1) + 'px';
+        unregisterInstance(this);
     }
 
     render() {
