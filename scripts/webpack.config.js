61548d28021298ba1e174d9b97a59147e63d4739
Evan Jacobs <glitterbyte@gmail.com>
2017-01-19 00:47:10 -0500

    Fix HMR

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -6,8 +6,8 @@ module.exports = {
     entry: path.resolve(__dirname, '../site/index.js'),
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, '../site/public/assets'),
-        publicPath: path.resolve(__dirname, '../site/public'),
+        path: path.resolve(__dirname, '../site/public/assets/'),
+        publicPath: 'assets/',
     },
     module: {
         rules: [{
@@ -18,17 +18,16 @@ module.exports = {
             loader: 'raw-loader',
         }, {
             test: /\.styl$/,
-            loader: 'style-loader!css-loader!stylus-loader',
+            loader: 'style-loader!css-loader!stylus-loader?sourceMap',
         }],
     },
     devServer: {
-        contentBase: path.resolve(__dirname, '../site/public'),
         compress: true,
         historyApiFallback: true,
         host: '0.0.0.0',
         publicPath: '/assets/',
     },
-    devtool: 'source-map',
+    devtool: 'inline-source-map',
     externals: {
         'lodash': '_',
         'react': 'React',
