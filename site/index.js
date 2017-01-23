33e212623fd7f97fb5d7b06bb279868f7e9e9b63
Evan Jacobs <glitterbyte@gmail.com>
2017-01-23 18:56:42 -0500

    Make the starfield JSX static

--- site/index.js
+++ site/index.js
@@ -31,18 +31,12 @@ const components = _.keys(Boundless).map((prettyName) => {
     };
 });
 
-const Starfield = () => (
+const starfield = (
     <div className='starfield'>
         <div className='starfield-seed-1' />
         <div className='starfield-seed-2' />
         <div className='starfield-seed-3' />
     </div>
-);
-
-const svgCaretComponent = (
-    <svg width='1792' height='1792' viewBox='0 0 1792 1792' xmlns='http://www.w3.org/2000/svg'>
-        <path d='M1408 704q0 26-19 45l-448 448q-19 19-45 19t-45-19l-448-448q-19-19-19-45t19-45 45-19h896q26 0 45 19t19 45z'/>
-    </svg>
 );
 
 const repositoryURL = 'https://github.com/bibliotech/uikit';
@@ -102,7 +96,7 @@ class Container extends React.PureComponent {
     renderSplash() {
         return (
             <section className='splash'>
-                <Starfield />
+                {starfield}
 
                 <div className='splash-inner'>
                     <h1>boundless</h1>
@@ -157,7 +151,7 @@ class Container extends React.PureComponent {
                 </main>
 
                 <footer className='boundless-footer'>
-                    <Starfield />
+                    {starfield}
 
                     <div>
                         <strong>boundless</strong>&nbsp;is developed in partnership with&nbsp;<a href='http://enigma.io'>enigma</a>
